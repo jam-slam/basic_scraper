a434a52e84914a6b61084fcc604938a496306eae
James H <122468839+jam-slam@users.noreply.github.com>
2024-01-09 17:01:14 +0000

    Update IAP_scraper.py

--- IAP_scraper.py
+++ IAP_scraper.py
@@ -5,29 +5,28 @@ from bs4 import BeautifulSoup
  
 #Remove hashes below if you want to read URLs from a CSV    
 #df = pd.read_csv('.csv') 
-#URL_list = df.values.tolist()
-#print(URL_list)
+#app_store_urls = df.values.tolist()
+#print(app_store_urls)
 
-#Use this to test print out - remove '_2' to read from the orignal list above)
-URL_list_2 = ['https://apps.apple.com/us/app/beat-maker-go-make-music/id1141835258',
+app_store_urls = ['https://apps.apple.com/us/app/beat-maker-go-make-music/id1141835258',
               'https://apps.apple.com/us/app/deezer-music-podcast-player/id292738169']
  
-IAP_overall = []
-for url in URL_list_2:
-    asp = requests.get(url)
+iap_list = []
+for urls in app_store_urls:
+    asp = requests.get(urls)
     asp_parsed = BeautifulSoup(asp.content, "html.parser")
-    IAP_list = []
-    for IAP in asp_parsed.find_all("li", class_="list-with-numbers__item"):
-        IAP_list.append(IAP.get_text())
+    iap_list = []
+    for iaps in asp_parsed.find_all("li", class_="list-with-numbers__item"):
+        iap_list.append(IAP.get_text())
         list_1 = [a.strip("\n") for a in IAP_list]
-        clean_IAP_list = [b.split("\n") for b in list_1]
-        clean_IAP_list.insert(0,url)
-    IAP_overall.append(clean_IAP_list)
+        clean_iap_list = [b.split("\n") for b in list_1]
+        clean_iap_list.insert(0,url)
+    iap_final.append(clean_IAP_list)
  
 #Use for test, delete later
-print(IAP_overall)
+print(iap_final)
 
-df = pd.DataFrame(IAP_overall)
+df = pd.DataFrame(iap_final)
 
 #Save to CSV
 df.to_csv('IAP.csv')
